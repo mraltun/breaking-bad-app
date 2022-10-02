c071a37f7979af18a2bd24baa74b97aacebf109d
Murat Altun <mraltun@gmail.com>
2022-10-02 12:38:54 +0300

    Added Home and Header pages. Setup TailwindCSS

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+
+const Home = () => {
+  const [character, setCharacter] = useState("");
+
+  return <div>Home</div>;
+};
+
+export default Home;
