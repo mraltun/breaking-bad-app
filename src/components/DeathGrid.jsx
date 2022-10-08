a4e1aad179a385f25d409a0dfda679d1491c8f1b
Murat Altun <mraltun@gmail.com>
2022-10-08 12:30:24 +0300

    DeathGrid improvements

--- src/components/DeathGrid.jsx
+++ src/components/DeathGrid.jsx
@@ -44,17 +44,31 @@ const DeathGrid = () => {
   return (
     <section>
       {data && (
-        <div className='h-screen flex items-center justify-center'>
-          <card className='flex flex-col rounded-lg shadow-lg w-1/2  bg-white'>
+        <div className='flex items-center justify-center min-h-screen'>
+          <div className='flex flex-col rounded-lg shadow-lg w-1/2  bg-white'>
             <div className='flex justify-center'>
-              <img src={crimeScene} />
+              <img src={crimeScene} alt='' className='w-full h-52' />
             </div>
-            <p className='text-center text-gray-800 text-4xl mt-8'>
+            <h1 className='text-center text-gray-800 text-5xl mt-5 mb-6'>
               Death Details
+            </h1>
+            <p className='text-center text-gray-800 '>Deceased: {data.death}</p>
+            <p className='text-center text-gray-800 '>
+              Killer: {data.responsible}
             </p>
-            The Deceased
-            <p className='text-center text-gray-700 font-light'>{data.death}</p>
-          </card>
+            <p className='text-center text-gray-800 '>
+              Cause of death: {data.cause}
+            </p>
+            <p className='text-center text-gray-800 '>
+              Season: {data.season} Episode: {data.episode}
+            </p>
+            <p className='text-center text-gray-800 '>
+              Last words: "{data.last_words}"
+            </p>
+            <p className='text-center text-gray-800  mb-10'>
+              How many deaths: {data.number_of_deaths}
+            </p>
+          </div>
         </div>
       )}
     </section>
